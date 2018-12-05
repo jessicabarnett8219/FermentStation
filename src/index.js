f8b01abc59f939ee797521230a49aa3165c1f72a
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-12-05 14:14:48 -0600

    set up component file structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './FermentStation';
+import App from './components/FermentStation';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
