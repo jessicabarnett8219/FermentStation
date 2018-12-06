12f8cb0d9159f19795e602ab09555b194f5ccb85
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-12-05 19:27:54 -0600

    set up Semantic UI

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,15 @@
 import { Route, Redirect } from "react-router-dom"
 import React, { Component } from "react"
 
+
 class ApplicationViews extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <h1>Hello!</h1>
-      </React.Fragment>
+      <div>
+        <h1>Hello</h1>
+        <button className='ui button' role='button'>Click Here</button>
+      </div>
     )
   }
 }
