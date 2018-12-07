f39e52d5a3f645e7be85356f2f2094d566425152
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-12-07 10:50:57 -0600

    fixed bug with batch detail rendering before state was set

--- src/components/batch/form/ReviewForm.js
+++ src/components/batch/form/ReviewForm.js
@@ -1,23 +1,41 @@
 import React, { Component } from "react"
 import { Grid, Form, Button, Header } from 'semantic-ui-react'
+import APIManager from "../../../modules/APIManager"
+
 
 
 class SampleForm extends Component {
+  state = {
+    batchName: "",
+    startDate: "",
+    batchId: ""
+  }
+
+  componentDidMount() {
+    const { batchId } = this.props.match.params
+    APIManager.getEntry("batches", batchId)
+      .then(batch => {
+        this.setState({
+          batchName: batch.name,
+          batchId: batch.id,
+          bottleDate: batch.bottleDate
+        })
+      })
+  }
+
   render() {
     return (
       <Grid columns={1} padded={true}>
         <Grid.Column>
-        <Header as="h1" textAlign="center">Sample a Batch</Header>
-          <p><span className="form_label">Batch Name: </span>My First Ferment</p>
-          <p><span className="form_label">Bottled Since: </span>12/20/2018</p>
+          <Header as="h1" textAlign="center">Review {this.state.batchName}<Header.Subheader>Bottled Since: {this.state.bottleDate}</Header.Subheader></Header>
           <Form>
-            <Form.Input fluid label="Sample Date" type="date" />
-            <Form.Group inline>
+            <Form.Input fluid label="Completion Date" type="date" />
+            <Form.Group>
               <label>Rating</label>
-              <Form.Radio label="Thumbs Up" />
-              <Form.Radio label="Thumbs Down" />
+              <Form.Radio label="Positive" />
+              <Form.Radio label="Negative" />
             </Form.Group>
-            <Form.Input label="Review Notes" type="text" />
+            <Form.Input label="Review" type="text" />
             <Button>Cancel</Button>
             <Button>Save</Button>
           </Form>
--- src/components/batch/list/CompleteBatchesList.js
+++ src/components/batch/list/CompleteBatchesList.js
@@ -1,35 +1,42 @@
 import React, { Component } from "react"
 import { Grid, Button, Header, List } from 'semantic-ui-react'
 import { Link } from "react-router-dom"
+import APIManager from "../../../modules/APIManager"
 
 
 class PastBatchesList extends Component {
+
+  state = {
+    batches: []
+  }
+
+  componentDidMount() {
+    APIManager.getAllEntries("batches", "/?user=1&status=3")
+      .then(batches => this.setState({ batches: batches }))
+  }
+
   render() {
     return (
-      <Grid columns={1} padded={true}>
+      <Grid columns={1} padded>
         <Grid.Column>
-          <Header as="h1" textAlign="center">Completed</Header>
+          <Header as="h1" textAlign="center">Completed Batches</Header>
           <List divided>
-            <List.Item>
-              <List.Content floated='right'>
-              <Link to="./batches/1"><Button>Details</Button></Link>
-              </List.Content>
-              <List.Content><Header size="medium">My First Ferment <Header.Subheader>Completed On: 12/21/18</Header.Subheader></Header></List.Content>
-            </List.Item>
-            <List.Item>
-              <List.Content floated='right'>
-              <Link to="./batches/1"><Button>Details</Button></Link>
-              </List.Content>
-              <List.Content><Header size="medium">Grape Soda <Header.Subheader>Completed On: 12/20/18</Header.Subheader></Header></List.Content>
-            </List.Item>
-            <List.Item>
-              <List.Content floated='right'>
-              <Link to="./batches/1"><Button>Details</Button></Link>
-              </List.Content>
-              <List.Content><Header size="medium">Ginger Blueberry <Header.Subheader>Completed On: 12/20/18</Header.Subheader></Header></List.Content>
-            </List.Item>
-
+            {
+              this.state.batches.map(batch => {
+                return <List.Item key={batch.id}>
+                  <List.Content floated='right'>
+                    <Link to={`/batches/${batch.id}`} {...this.props}><Button>Details</Button></Link>
+                  </List.Content>
+                  <List.Content>
+                    <Header size="medium">{batch.name}
+                    <Header.Subheader>Completed On: {batch.completeDate}</Header.Subheader>
+                    </Header>
+                  </List.Content>
+                </List.Item>
+              })
+            }
           </List>
+
         </Grid.Column>
       </Grid>
     )
