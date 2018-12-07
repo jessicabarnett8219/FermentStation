197cccb9bca67034c2bc2c0c28531d8059080083
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-12-07 11:46:16 -0600

    expanded type in order to print type name to the page

--- src/components/batch/detail/BatchDetail.js
+++ src/components/batch/detail/BatchDetail.js
@@ -16,7 +16,7 @@ class BatchDetail extends Component {
 
   componentDidMount() {
     const { batchId } = this.props.match.params
-    APIManager.getEntry("batches", batchId)
+    APIManager.getEntry("batches", batchId, "?_expand=type")
       .then(batchObj => {
         this.setState({ batch: batchObj }, () => this.setState({ initialized: true }, () => console.log(this.state)))
       })
@@ -26,16 +26,16 @@ class BatchDetail extends Component {
     if (this.state.initialized === true) {
       if (this.state.batch.status === 1) {
         return (
-          <BrewingDetail name={this.state.batch.name} type={this.state.batch.typeId} startDate={this.state.batch.startDate} starterIngredients={this.state.batch.starterIngredients} batchId={this.state.batch.id} bottleDate={this.state.batch.bottleDate}/>
+          <BrewingDetail name={this.state.batch.name} type={this.state.batch.type.name} startDate={this.state.batch.startDate} starterIngredients={this.state.batch.starterIngredients} batchId={this.state.batch.id} bottleDate={this.state.batch.bottleDate}/>
         )
       } else if (this.state.batch.status === 2) {
         return (
-          <BottledDetail name={this.state.batch.name} type={this.state.batch.typeId} bottleDate={this.state.batch.bottleDate} starterIngredients={this.state.batch.starterIngredients} bottleIngredients={this.state.batch.bottleIngredients} startDate={this.state.batch.startDate}batchId={this.state.batch.id} completeDate={this.state.batch.completeDate}/>
+          <BottledDetail name={this.state.batch.name} type={this.state.batch.type.name} bottleDate={this.state.batch.bottleDate} starterIngredients={this.state.batch.starterIngredients} bottleIngredients={this.state.batch.bottleIngredients} startDate={this.state.batch.startDate}batchId={this.state.batch.id} completeDate={this.state.batch.completeDate}/>
         )
       }
       else if (this.state.batch.status === 3) {
         return (
-          <CompletedDetail name={this.state.batch.name} type={this.state.batch.typeId} completeDate={this.state.batch.completeDate} startDate={this.state.batch.startDate} bottleDate={this.state.batch.bottleDate} starterIngredients={this.state.batch.starterIngredients} bottleIngredients={this.state.batch.bottleIngredients} batchId={this.state.batch.id} rating={this.state.batch.rating} review={this.state.batch.review}/>
+          <CompletedDetail name={this.state.batch.name} type={this.state.batch.type.name} completeDate={this.state.batch.completeDate} startDate={this.state.batch.startDate} bottleDate={this.state.batch.bottleDate} starterIngredients={this.state.batch.starterIngredients} bottleIngredients={this.state.batch.bottleIngredients} batchId={this.state.batch.id} rating={this.state.batch.rating} review={this.state.batch.review}/>
         )
       }
     }
