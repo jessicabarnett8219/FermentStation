bb58b256c742fd0cc1caaa5199a1e97bad0eac8f
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-12-12 12:13:26 -0600

    finished basic styling for soft-demo

--- src/components/navigation/MainMenu.js
+++ src/components/navigation/MainMenu.js
@@ -9,22 +9,22 @@ class MainMenu extends Component {
 
     return (
       <div className="text-align-center">
-        <h1 className="padding-vertical-s">Welcome!</h1>
-        <div className="background-secondary-500 no-margin padding-vertical-l">
+        <h1 className="padding-vertical-s color-info">Welcome!</h1>
+        <div className="background-info no-margin padding-vertical-l">
           <Link to="/new-batch" className="link display-title">Start a New Batch</Link>
         </div>
 
 
-        <div className="background-secondary-600 no-margin padding-vertical-l flex-1-0-auto">
+        <div className="background-info-600 no-margin padding-vertical-l flex-1-0-auto">
           <Link to="/in-progress-list" className="link display-title">In-Progress</Link>
         </div>
-        <div className="background-secondary-700 no-margin padding-vertical-l flex-1-0-auto">
+        <div className="background-info-700 no-margin padding-vertical-l flex-1-0-auto">
           <Link to="/completed-list" className="link display-title">Completed</Link>
         </div>
 
 
-        <div className="background-secondary-800 no-margin padding-vertical-l">
-          <button className="button-text color-white display-title" onClick={
+        <div className="background-info-800 no-margin padding-vertical-l color-white">
+          <button className="button-text display-title" onClick={
             () => {
               sessionStorage.clear() || localStorage.clear()
               this.props.history.push("/welcome")
