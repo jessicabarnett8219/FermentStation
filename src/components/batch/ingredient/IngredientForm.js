dda3843e8bc66069bbcc9e8d2453011c84df5bd1
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-12-17 11:07:15 -0600

    added tea selection to ingredients form and separated state out for specific category

--- src/components/batch/ingredient/IngredientForm.js
+++ src/components/batch/ingredient/IngredientForm.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import APIManager from "../../../modules/APIManager"
 import NavBar from "../../navigation/NavBar"
 import SugarSelection from "./SugarSelection"
+import TeaSelection from "./TeaSelection"
 
 // TODO break out selected ingredient list into a separate component so that it can be used for all categories. pass category ids to ingredient selection so that it can be used for all categories. Repeat for all categories. Send up conditional rendering based on kombucha vs kefir. deal with save button.
 
@@ -9,22 +10,23 @@ class IngredientForm extends Component {
 
   state = {
     batchId: "",
-    currentIngredient: 1,
-    allIngredients: [],
+    currentIngredient: "",
+    selectedSugars: [],
+    selectedTeas: [],
   }
 
   componentDidMount() {
     const { batchId } = this.props.match.params
-    this.setState({batchId: +batchId})
+    this.setState({ batchId: +batchId })
   }
 
   getAllIngredients = () => {
     APIManager.getAllEntries("batches-ingredients", `?batchId=${this.state.batchId}&_expand=ingredient`)
-    .then(ingredients => this.setState({allIngredients: ingredients}))
+      .then(sugars => this.setState({ selectedSugars: sugars }))
   }
 
   handleIngredientSelection = (evt) => {
-    this.setState({currentIngredient: parseInt(evt.target.value)})
+    this.setState({ currentIngredient: parseInt(evt.target.value) })
   }
 
   constructbatchIngredient = () => {
@@ -38,43 +40,54 @@ class IngredientForm extends Component {
   handleSaveIngredient = () => {
     let newbatchIngredient = this.constructbatchIngredient()
     APIManager.addEntry("batches-ingredients", newbatchIngredient)
-    .then(() => this.getAllIngredients())
+      .then(() => this.getAllIngredients())
   }
 
   deleteIngredient = (id) => {
     APIManager.deleteEntry("batches-ingredients", id)
-    .then(() => this.getAllIngredients())
+      .then(() => this.getAllIngredients())
   }
 
   render() {
-      return (
-        <div>
-          <NavBar {...this.props}/>
-          <div className="container">
+    return (
+      <div>
+        <NavBar {...this.props} />
+        <div className="container">
           <h1 className="text-align-center">Add Ingredients</h1>
-          <h3>Sugar</h3>
-          <SugarSelection handleIngredientSelection={this.handleIngredientSelection} />
-          <button onClick={() => {
+          <div>
+            <h3>Sugar</h3>
+            <SugarSelection handleIngredientSelection={this.handleIngredientSelection} />
+            <button onClick={() => {
               this.handleSaveIngredient()
             }}>Add</button>
-              <div>
-                <ul>
+            <div>
+              <ul>
                 {
-                  this.state.allIngredients.map(ingredientObj => {
+                  this.state.selectedSugars.map(ingredientObj => {
                     return <li key={ingredientObj.id}>{ingredientObj.ingredient.name}
-                    <button className="button-xs" onClick={() => {
-                      this.deleteIngredient(ingredientObj.id)
-                    }}>Delete</button>
+                      <button className="button-xs" onClick={() => {
+                        this.deleteIngredient(ingredientObj.id)
+                      }}>Delete</button>
                     </li>
                   })
                 }
-                </ul>
-              </div>
-              <button className="button info margin-left-xxs margin-top-xxs" onClick={() => {
-              }}>Save</button>
+              </ul>
+            </div>
+
+            <div>
+              <h3>Tea</h3>
+              <TeaSelection handleIngredientSelection={this.handleIngredientSelection} />
+              <button onClick={() => {
+                this.handleSaveIngredient()
+              }}>Add</button>
+
             </div>
           </div>
-      )
+          <button className="button info margin-left-xxs margin-top-xxs" onClick={() => {
+          }}>Save</button>
+        </div>
+      </div>
+    )
 
 
   }
---
+++ src/components/batch/ingredient/TeaSelection.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react"
+import APIManager from "../../../modules/APIManager"
+
+
+class TeaSelection extends Component {
+
+  state = {
+    teaOptions: []
+  }
+
+  componentDidMount() {
+    APIManager.getAllEntries("ingredients", "?categoryId=3")
+      .then(options => this.setState({
+        teaOptions: options
+      }))
+
+  }
+  render() {
+    return (
+      <div>
+        <label className="select" htmlFor="sugarId">
+          <select id="ingredientId" name="ingredientId" onChange={
+                  (evt) => {
+                    this.props.handleIngredientSelection(evt)
+                  }
+                }>
+            {
+              this.state.teaOptions.map(tea => {
+                return <option key={tea.id} value={tea.id} >
+                  {tea.name}
+                </option>
+              })
+            }
+          </select>
+        </label>
+      </div>
+
+    )
+  }
+}
+
+export default TeaSelection
