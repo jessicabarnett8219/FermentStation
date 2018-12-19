73b7aab105033359a85f1d42172965fbbadcf6bd
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-12-19 11:49:34 -0600

    added ternary for date labels based on status

--- src/components/batch/edit-form/BasicEdit.js
+++ src/components/batch/edit-form/BasicEdit.js
@@ -7,24 +7,27 @@ class BasicEdit extends Component {
     return (
       <React.Fragment>
         <h1 className="text-align-center">Edit Batch</h1>
-          <label htmlFor="editName">Name</label>
-          <input id="editName" type="text" defaultValue={this.props.batch.name} onChange={
-            (evt) => { this.props.handleFieldChange(evt) }
-          } />
+        <label htmlFor="editName">Name</label>
+        <input id="editName" type="text" defaultValue={this.props.batch.name} onChange={
+          (evt) => { this.props.handleFieldChange(evt) }
+        } />
 
-          <BatchTypeEditForm batchType={this.props.batch.typeId} handleFieldChangeRadio={this.props.handleFieldChangeRadio} typeOptions={this.props.typeOptions} />
+        <BatchTypeEditForm batchType={this.props.batch.typeId} handleFieldChangeRadio={this.props.handleFieldChangeRadio} typeOptions={this.props.typeOptions} />
 
-          <label htmlFor="editStartDate">Start Date</label>
-          <input type="date" id="editStartDate" defaultValue={this.props.batch.startDate} onChange={
-            (evt) => { this.props.handleFieldChange(evt) }
-          } />
+        <label htmlFor="editStartDate">Start Date</label>
+        <input type="date" id="editStartDate" defaultValue={this.props.batch.startDate} onChange={
+          (evt) => { this.props.handleFieldChange(evt) }
+        } />
 
-          <label htmlFor="editBottleDate">Expected Bottling Date</label>
-          <input type="date" id="editBottleDate" defaultValue={this.props.batch.bottleDate} onChange={
-            (evt) => { this.props.handleFieldChange(evt) }
-          } />
+        <label htmlFor="editBottleDate"> {
+          this.props.batch.status === 1 ? "Expected Bottling Date" : "Bottled On"
+        }
+        </label>
+        <input type="date" id="editBottleDate" defaultValue={this.props.batch.bottleDate} onChange={
+          (evt) => { this.props.handleFieldChange(evt) }
+        } />
 
-    </React.Fragment>
+      </React.Fragment>
     )
   }
 }
--- src/components/batch/edit-form/BottledEdit.js
+++ src/components/batch/edit-form/BottledEdit.js
@@ -5,7 +5,11 @@ class BottledEdit extends Component {
   render() {
     return (
       <React.Fragment>
-          <label htmlFor="editCompleteDate">Expected Completion Date</label>
+          <label htmlFor="editCompleteDate">
+          {this.props.batch.status === 3 ? "Completed On" :
+          "Expected Completion Date"
+          }
+          </label>
           <input type="date" id="editCompleteDate" defaultValue={this.props.batch.completeDate} onChange={
             (evt) => { this.props.handleFieldChange(evt) }
           } />
